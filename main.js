ff7a7eef40cacc3124f96a48ad894a85fbb771c4
0xAquaWolf <0xaquawolf@protonmail.com>
2024-05-31 16:04:26 +0700

    player paddle movement and ball bouncing

--- main.js
+++ main.js
@@ -3,9 +3,20 @@ import "./style.css";
 const canvas = document.querySelector("#gameCanvas");
 const canvasContext = canvas.getContext("2d");
 let ballX = 50;
-let ballY = 100;
+let ballY = 50;
 let ballSpeedX = 10;
 let ballSpeedY = 5;
+
+// human player
+let paddle1Y = 250;
+let paddle1X = 0;
+
+// ai player
+let paddle2Y = 0;
+let paddle2X = 0;
+
+const PADDLE_HEIGHT = 100;
+const PADDLE_WIDTH = 10;
 
 window.onload = () => {
   console.log("Make sure to like and subscribe :)");
@@ -16,7 +27,35 @@ window.onload = () => {
     moveEverything();
     drawEverything();
   }, 1000 / framesPerSecond);
+
+  canvas.addEventListener("mousemove", (e) => {
+    let { x, y } = calculateMousePosition(e);
+    console.log({ x, y });
+    paddle1Y = y - PADDLE_HEIGHT / 2;
+    paddle1X = x;
+  });
 };
+
+function calculateMousePosition(e) {
+  let rect = canvas.getBoundingClientRect();
+  let root = document.documentElement;
+  let mouseX = e.clientX - rect.left - root.scrollLeft;
+  let mouseY = e.clientY - rect.top - root.scrollTop;
+
+  let mouseCords = {
+    x: mouseX,
+    y: mouseY,
+  };
+
+  // console.log({ mouseCords });
+
+  return mouseCords;
+}
+
+function ballReset() {
+  ballX = canvas.width / 2;
+  ballY = canvas.height / 2;
+}
 
 function colorRect(leftX, topY, width, height, drawColor) {
   canvasContext.fillStyle = drawColor;
@@ -25,16 +64,24 @@ function colorRect(leftX, topY, width, height, drawColor) {
 
 function moveEverything() {
   ballX += ballSpeedX;
-  ballSpeedX += 0.5; // added accelration
+  ballY += ballSpeedY;
+
   if (ballX > canvas.width) {
     ballSpeedX = -ballSpeedX;
-  } else if (ballX < 0) {
-    ballSpeedX = -ballSpeedX;
+  }
+
+  if (ballX < 0) {
+    if (ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT) {
+      // if the ball is inside of the paddle
+      ballSpeedX = -ballSpeedX;
+    } else ballReset();
   }
 
   if (ballY > canvas.height) {
     ballSpeedY = -ballSpeedY;
-  } else if (ballY < 0) {
+  }
+
+  if (ballY < 0) {
     ballSpeedY = -ballSpeedY;
   }
 }
@@ -48,17 +95,12 @@ function drawBall(centerX, centerY, radius, drawColor) {
 }
 
 function drawEverything() {
-  console.log({ ballX });
+  // console.log({ ballX });
   // black canvas
   colorRect(0, 0, canvas.width, canvas.height, "#181825");
 
-  drawBall(ballX, ballY, 10, "#cba6f7");
-
-  const whiteBoxWidth = 10;
-  const whiteBoxHeight = 100;
-
-  const paddleYPosition = canvas.height / 2 - whiteBoxHeight / 2;
+  drawBall(ballX, ballY, 10, "#f38ba8");
 
   // left paddle
-  colorRect(0, paddleYPosition, whiteBoxWidth, whiteBoxHeight, "#cdd6f4");
+  colorRect(0, paddle1Y, PADDLE_WIDTH, PADDLE_HEIGHT, "#cdd6f4");
 }
