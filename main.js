fe443ffe2d485211faa7528a49a8fd1cff091c4a
0xAquaWolf <0xaquawolf@protonmail.com>
2024-05-31 15:19:01 +0700

    updated ball movement

--- main.js
+++ main.js
@@ -3,39 +3,62 @@ import "./style.css";
 const canvas = document.querySelector("#gameCanvas");
 const canvasContext = canvas.getContext("2d");
 let ballX = 50;
-let ballY = 50;
+let ballY = 100;
+let ballSpeedX = 10;
+let ballSpeedY = 5;
 
 window.onload = () => {
   console.log("Make sure to like and subscribe :)");
   console.log({ canvas });
   console.log({ canvasContext });
-  const framesPerSecond = 30;
+  const framesPerSecond = 60;
   setInterval(() => {
     moveEverything();
     drawEverything();
   }, 1000 / framesPerSecond);
 };
 
+function colorRect(leftX, topY, width, height, drawColor) {
+  canvasContext.fillStyle = drawColor;
+  canvasContext.fillRect(leftX, topY, width, height);
+}
+
 function moveEverything() {
-  ballX += 10;
+  ballX += ballSpeedX;
+  ballSpeedX += 0.5; // added accelration
+  if (ballX > canvas.width) {
+    ballSpeedX = -ballSpeedX;
+  } else if (ballX < 0) {
+    ballSpeedX = -ballSpeedX;
+  }
+
+  if (ballY > canvas.height) {
+    ballSpeedY = -ballSpeedY;
+  } else if (ballY < 0) {
+    ballSpeedY = -ballSpeedY;
+  }
+}
+
+function drawBall(centerX, centerY, radius, drawColor) {
+  // game ball
+  canvasContext.fillStyle = drawColor;
+  canvasContext.beginPath();
+  canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true); // 6.283185307179586
+  canvasContext.fill();
 }
 
 function drawEverything() {
   console.log({ ballX });
-  canvasContext.fillStyle = "black";
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  // black canvas
+  colorRect(0, 0, canvas.width, canvas.height, "#181825");
 
-  canvasContext.fillStyle = "purple";
-  canvasContext.fillRect(ballX, ballY, 20, 20);
-
-  canvasContext.fillStyle = "white";
+  drawBall(ballX, ballY, 10, "#cba6f7");
 
   const whiteBoxWidth = 10;
   const whiteBoxHeight = 100;
 
-  console.log(canvas.height);
-
   const paddleYPosition = canvas.height / 2 - whiteBoxHeight / 2;
 
-  canvasContext.fillRect(0, paddleYPosition, whiteBoxWidth, whiteBoxHeight);
+  // left paddle
+  colorRect(0, paddleYPosition, whiteBoxWidth, whiteBoxHeight, "#cdd6f4");
 }
