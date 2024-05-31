655a522b25680f10961833bd339c565f8392c9a2
0xAquaWolf <0xaquawolf@protonmail.com>
2024-05-31 12:59:00 +0700

    first init

---
+++ main.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+// This is where i will write the game logic
+
+window.onload = () => {
+  const canvas = document.querySelector("#gameCanvas");
+  console.log({ canvas });
+
+  console.log("Hello, Youtube!");
+};
